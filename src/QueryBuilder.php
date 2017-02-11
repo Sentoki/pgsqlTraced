3383957ae796e8ac88f40832d1f013063fbed527
Egor Petrov <e.petrov@freematiq.com>
2017-02-11 20:09:58 +0700

    one more commit

--- src/QueryBuilder.php
+++ src/QueryBuilder.php
@@ -3,12 +3,19 @@
 namespace EgorPetrov\pgsqlTraced;
 
 
-class QueryBuilder extends yii\db\pgsql\QueryBuilder
+class QueryBuilder extends \yii\db\pgsql\QueryBuilder
 {
 
     public function build($query, $params = [])
     {
         list($sql, $params) = parent::build($query, $params);
+        /**
+         * add trace
+         */
+        $e = new \Exception();
+        $trace = $e->getTraceAsString();
+        $trace = preg_replace("/\n/", "\n--", $trace);
+        $sql = $sql . "\n--this is a trace\n--$trace";
         return [$sql, $params];
     }
 
--- src/Schema.php
+++ src/Schema.php
@@ -2,11 +2,7 @@
 
 namespace EgorPetrov\pgsqlTraced;
 
-use yii\db\Expression;
-use yii\db\TableSchema;
-use yii\db\ColumnSchema;
-
-class Schema extends yii\db\pgsql\Schema
+class Schema extends \yii\db\pgsql\Schema
 {
     public function createQueryBuilder()
     {
